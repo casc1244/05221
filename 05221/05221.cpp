2e54782d4d7a65fac9af63709464b08646a741d5
1356602646 <1356602646@qq.com>
2020-05-22 14:56:14 +0800

    05221.cpp

--- 05221/05221.cpp
+++ 05221/05221.cpp
@@ -1,20 +1,57 @@
-﻿// 05221.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>  
+#include<opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
 
-#include <iostream>
+int bgSub_demo()
+{
+	VideoCapture capVideo(0);
+//如果打开失败
+	if (!capVideo.isOpened()) {
+		std::cout << "Unable to open video!" << std::endl;
+		return -1;
+	}
+   //计数
+	int cnt = 0;
+	Mat frame;
+	Mat bgMat;
+	Mat subMat;
+	Mat bny_subMat;
+	Mat bgModelMat;
 
+	while (1) {
+		capVideo >> frame;
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
+		if (cnt == 0) {
+			//第一帧，获得背景图像
+			frame.copyTo(bgMat);
+		}
+		else {
+			//第二差分
+			//相减
+			absdiff(frame, bgMat, subMat);
+			//二值化
+			threshold(subMat, bny_subMat, 20, 255, CV_THRESH_BINARY);
+
+			imshow("b_subMat", bny_subMat);
+			imshow("frame", frame);
+			waitKey(30);
+		}
+		cnt++;
+
+	}
+	return 0;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	//开始计时
+	double start = static_cast<double>(cvGetTickCount());
+	bgSub_demo();
+	//结束计时
+	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
+	//显示时间
+	cout << "processing time:" << time / 1000 << "ms" << endl;
+	//按任意键结束程序
+	system("pause");
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
